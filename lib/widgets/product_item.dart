3b95cabc4ece82883af93021eacf1fd00cd8671a
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-14 23:42:24 +0000

    add radius and color for product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,18 +9,23 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageUrl, fit: BoxFit.cover),
-      footer: GridTileBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.favorite),
-        ),
-        backgroundColor: Colors.black54,
-        title: Text(title, textAlign: TextAlign.center),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(title, textAlign: TextAlign.center),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
         ),
       ),
     );
