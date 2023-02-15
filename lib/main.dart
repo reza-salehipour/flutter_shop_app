835608c20af8d47233e79dba336761134f7b199b
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 09:51:51 +0000

    add ChangeNotifierProvider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: ((context) => ProductDetailScreen())
-      },
+    return ChangeNotifierProvider(
+      create: (context) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: ((context) => ProductDetailScreen())
+        },
+      ),
     );
   }
 }
