771e01794eea6d9f4b5f904fb4cafc8463a47c73
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 14:55:28 +0000

    adding filter logic base on provider

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 import '../widgets/products_grid.dart';
+
+enum filterOptions { Favorites, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productContainer = Provider.of<ProductsProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyShop'),
+        title: const Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (filterOptions selectedValue) {
+              if (selectedValue == filterOptions.Favorites) {
+                productContainer.showFavoriteOnly();
+              } else {
+                productContainer.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: filterOptions.Favorites,
+                child: Text('Only Favorites'),
+              ),
+              const PopupMenuItem(
+                  child: Text('Show All'), value: filterOptions.All)
+            ],
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
