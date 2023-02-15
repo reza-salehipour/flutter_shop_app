b76d02620c87f3e9767b1d13a8ed273d6e04b249
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 11:40:50 +0000

    change from normal state passing to provider base and fixing favorite btn

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem({required this.id, required this.title, required this.imageUrl});
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).accentColor,
           ),
           backgroundColor: Colors.black87,
-          title: Text(title, textAlign: TextAlign.center),
+          title: Text(product.title, textAlign: TextAlign.center),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             onPressed: () {},
@@ -30,10 +30,10 @@ class ProductItem extends StatelessWidget {
         ),
         child: GestureDetector(
             onTap: () {
-              Navigator.of(context)
-                  .pushNamed(ProductDetailScreen.routeName, arguments: id);
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
             },
-            child: Image.network(imageUrl, fit: BoxFit.cover)),
+            child: Image.network(product.imageUrl, fit: BoxFit.cover)),
       ),
     );
   }
