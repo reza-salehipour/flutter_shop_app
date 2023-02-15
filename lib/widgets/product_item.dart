c22429552ea56f23b644a21fec64423ea476b8a1
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 14:55:45 +0000

    add a consumer to work with

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,19 +7,21 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-          ),
+          leading: Consumer<Product>(
+              builder: (context, product, child) => IconButton(
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
+                    icon: Icon(product.isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border),
+                    color: Theme.of(context).accentColor,
+                  )),
           backgroundColor: Colors.black87,
           title: Text(product.title, textAlign: TextAlign.center),
           trailing: IconButton(
