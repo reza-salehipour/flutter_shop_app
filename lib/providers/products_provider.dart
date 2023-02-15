184689e61bbcd51fbf303c3c65ed3d0c0e338bcb
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 08:30:19 +0000

    add simple provider

---
+++ lib/providers/products_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductsProvider with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct() {
+    // _items.add(value);
+    notifyListeners();
+  }
+}
