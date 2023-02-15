9d250f1beb7ced62b538e89a051e11c851a6828c
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-15 08:07:54 +0000

    add routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.purple,
           accentColor: Colors.deepOrange),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: ((context) => ProductDetailScreen())
+      },
     );
   }
 }
