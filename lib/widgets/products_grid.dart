45f06a3b08f77ddf5c056a97d0ce10c08007ed51
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-16 08:12:18 +0000

    accept paramers here

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,10 +5,14 @@ import '../providers/products_provider.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavorites;
+  ProductsGrid(this.showFavorites);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<ProductsProvider>(context);
-    final products = productsData.items;
+    final products =
+        showFavorites ? productsData.favoritesItems : productsData.items;
     return GridView.builder(
         padding: const EdgeInsets.all(10.0),
         itemCount: products.length,
