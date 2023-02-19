a8446e56410ca2c2f187d728e8f78a0bc7040376
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-19 10:10:03 +0000

    use MultiProvider and add cart route

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,21 @@ import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
+
 import './providers/products_provider.dart';
+import './providers/cart_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProductsProvider()),
+        ChangeNotifierProvider(create: (context) => CartProvider())
+      ],
       // value: ProductsProvider(),
       child: MaterialApp(
         title: 'MyShop',
@@ -21,7 +27,8 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange),
         home: ProductsOverviewScreen(),
         routes: {
-          ProductDetailScreen.routeName: ((context) => ProductDetailScreen())
+          ProductDetailScreen.routeName: ((context) => ProductDetailScreen()),
+          CartScreen.routeName: ((context) => CartScreen())
         },
       ),
     );
