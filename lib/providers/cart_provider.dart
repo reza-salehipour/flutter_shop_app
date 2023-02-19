7f88b01a5d10df1ecdbac1ab26682232e5da457a
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-19 10:10:14 +0000

    add cart provider

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {required this.id,
+      required this.title,
+      required this.quantity,
+      required this.price});
+}
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartitem) {
+      total += cartitem.price * cartitem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(
+      {required String productId,
+      required double price,
+      required String title}) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity + 1,
+              price: existingCartItem.price));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price));
+      notifyListeners();
+    }
+  }
+}
