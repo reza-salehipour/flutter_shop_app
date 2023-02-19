846c12171df0294a300a08ee6bbb6de7eaad9f1e
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-19 10:12:13 +0000

    add add function to cart here

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,11 +3,14 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
+import '../providers/cart_provider.dart';
 
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<CartProvider>(context, listen: false);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,7 +29,12 @@ class ProductItem extends StatelessWidget {
           title: Text(product.title, textAlign: TextAlign.center),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(
+                  productId: product.id,
+                  price: product.price,
+                  title: product.title);
+            },
             color: Theme.of(context).accentColor,
           ),
         ),
