eefac4ccb2a9f620767af4148da7a2a817ed2b59
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-19 11:41:46 +0000

    add remove item function

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -55,4 +55,9 @@ class CartProvider with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
 }
