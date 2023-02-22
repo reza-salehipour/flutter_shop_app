7f347ed0fbf49c635132622830aaf88e1eb5778f
Reza Salehipour <rez.salehipour@gmail.com>
2023-02-22 14:18:33 +0000

    register OrdersScreen route

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
+import './screens/orders_screen.dart';
 
 import './providers/products_provider.dart';
 import './providers/cart_provider.dart';
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: ((context) => ProductDetailScreen()),
-          CartScreen.routeName: ((context) => CartScreen())
+          CartScreen.routeName: ((context) => CartScreen()),
+          OrdersScreen.routeName: ((context) => OrdersScreen())
         },
       ),
     );
