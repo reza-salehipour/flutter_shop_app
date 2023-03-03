f160abcc1a32afc649c9f6059edbda7e0536100e
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-03 12:25:44 +0000

    add modal in case of faluire

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -98,7 +98,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<ProductsProvider>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<void>(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: Text('An error occured!'),
+                  content: Text('Something went wronng'),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(_).pop();
+                        },
+                        child: Text('oKAY'))
+                  ],
+                ));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
