d07ec64eff00188560ad3b0727873cd9c029c68c
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-03 11:16:08 +0000

    send http req for add item with loader

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -67,16 +68,30 @@ class ProductsProvider with ChangeNotifier {
     return _items.firstWhere((product) => product.id == id);
   }
 
-  void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
+  Future<void> addProduct(Product product) {
+    final url = Uri.parse(
+        'https://shop-app-flutter-bca2d-default-rtdb.firebaseio.com/products.json');
+    return http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite
+            }))
+        .then((response) {
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      notifyListeners();
+    });
+
     // _items.insert(0,newProduct);
-    notifyListeners();
   }
 
   void updateProduct(String id, Product newProduct) {
