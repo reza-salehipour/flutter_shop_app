936aca43b7e56ad3a6dacc6fe25331cc55596f39
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-09 06:58:52 +0000

    comment dummy data and fetchAndSetProducts func

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -7,38 +7,38 @@ import './product.dart';
 class ProductsProvider with ChangeNotifier {
   // ignore: prefer_final_fields
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    )
+    // Product(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageUrl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // )
   ];
 
   // var _showFavoriteOnly = false;
@@ -95,6 +95,33 @@ class ProductsProvider with ChangeNotifier {
 
   //   // _items.insert(0,newProduct);
   // }
+
+  Future<void> fetchAndSetProducts() {
+    print('reza');
+    final url = Uri.parse(
+        'https://shop-app-flutter-bca2d-default-rtdb.firebaseio.com/products.json');
+    return http.get(url).then((response) {
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      print(extractedData);
+      final List<Product> loadedProducts = [];
+
+      extractedData.forEach(
+        (productId, productData) {
+          loadedProducts.add(Product(
+              id: productId,
+              title: productData['title'],
+              description: productData['description'],
+              price: productData['price'],
+              imageUrl: productData['imageUrl'],
+              isFavorite: productData['isFavorite']));
+        },
+      );
+      _items = loadedProducts;
+      notifyListeners();
+    }).catchError((error) {
+      throw error;
+    });
+  }
 
   Future<void> addProduct(Product product) async {
     final url = Uri.parse(
