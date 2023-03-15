1d53dcfd4054d050693272566f4c17c819e5a4f1
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-15 02:17:14 +0000

    fix a bug

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -31,6 +31,9 @@ class OrdersProvider with ChangeNotifier {
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
     extractedData.forEach((orderId, orderData) {
       loadedOrders.add(
         OrderItem(
@@ -46,7 +49,7 @@ class OrdersProvider with ChangeNotifier {
             dateTime: DateTime.parse(orderData['dateTime'])),
       );
     });
-    _orders = loadedOrders;
+    _orders = loadedOrders.reversed.toList();
     notifyListeners();
   }
 
