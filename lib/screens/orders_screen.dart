ed31eaeb0581d1772201dad38edab11c0eed4820
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-15 15:36:12 +0000

    add stateful widget inside a stateless widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,8 +6,30 @@ import '../providers/orders_provider.dart' show OrdersProvider;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static final routeName = '/orders';
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<OrdersProvider>(context, listen: false)
+          .fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +40,11 @@ class OrdersScreen extends StatelessWidget {
           title: const Text('Your Orders'),
         ),
         drawer: AppDrawer(),
-        body: ListView.builder(
-            itemBuilder: (_, i) => OrderItem(
-                  order: orderData.orders[i],
-                )));
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemBuilder: (_, i) => OrderItem(
+                      order: orderData.orders[i],
+                    )));
   }
 }
