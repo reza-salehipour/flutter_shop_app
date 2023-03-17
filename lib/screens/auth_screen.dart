b237e41f41256de8fc37b797017652846616d2c0
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-17 22:56:48 +0000

    fix typo

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -132,10 +132,10 @@ class _AuthCardState extends State<AuthCard> {
     });
     try {
       if (_authMode == AuthMode.Login) {
-        await Provider.of<Auth>(context, listen: false)
+        await Provider.of<AuthProvider>(context, listen: false)
             .login(_authData['email']!, _authData['password']!);
       } else {
-        await Provider.of<Auth>(context, listen: false)
+        await Provider.of<AuthProvider>(context, listen: false)
             .signup(_authData['email']!, _authData['password']!);
       }
     } on HttpException catch (error) {
