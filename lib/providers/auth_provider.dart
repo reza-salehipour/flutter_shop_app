b15fab3f68761db3bd2e226a314f5b9167dc9b0f
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-17 14:59:49 +0000

    fix class name

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 import '../models/http_exception.dart';
 
-class Auth with ChangeNotifier {
+class AuthProvider with ChangeNotifier {
   late String _token;
   late DateTime _expiryDate;
   late String _userId;
