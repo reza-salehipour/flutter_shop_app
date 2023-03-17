4c834451727b27e46d79560508346c0017af560f
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-17 22:54:38 +0000

    add auth to constructor of orders providers

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -20,6 +20,9 @@ class OrderItem {
 
 class OrdersProvider with ChangeNotifier {
   List<OrderItem> _orders = [];
+  final String? authToken;
+
+  OrdersProvider(this.authToken, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
@@ -27,7 +30,7 @@ class OrdersProvider with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url = Uri.parse(
-        'https://shop-app-flutter-bca2d-default-rtdb.firebaseio.com/orders.json');
+        'https://shop-app-flutter-bca2d-default-rtdb.firebaseio.com/orders.json?auth=$authToken');
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
