ab0b29b0c99eb658fda16f771d3a4ac12e5dd904
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-18 22:15:27 +0000

    add logout button and logic

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
+
+import '../providers/auth_provider.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -36,6 +39,15 @@ class AppDrawer extends StatelessWidget {
           onTap: () {
             Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName);
+          },
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.exit_to_app),
+          title: Text('Logout'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Provider.of<AuthProvider>(context, listen: false).logout();
           },
         )
       ],
