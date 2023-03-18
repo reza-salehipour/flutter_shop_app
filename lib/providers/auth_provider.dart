9d930a9233c644333692ad0f21c3f24bcd6280a2
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-18 22:15:50 +0000

    add logout function

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -59,4 +59,11 @@ class AuthProvider with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
+  }
 }
