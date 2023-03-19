fafd36298b60fd54bf9aa2bf6a13f0a2c3920531
Reza Salehipour <rez.salehipour@gmail.com>
2023-03-19 13:35:12 +0000

    add place holder image

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -61,7 +61,11 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                   arguments: product.id);
             },
-            child: Image.network(product.imageUrl, fit: BoxFit.cover)),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            )),
       ),
     );
   }
